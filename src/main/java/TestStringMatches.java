50faea08065e3ddaddfe1d29327e86b2d93812f4
Monkey <1466947023@qq.com>
2018-05-23 12:39:26 +0800

    初步认识Pattern和Matcher接口使用

--- src/main/java/TestStringMatches.java
+++ src/main/java/TestStringMatches.java
@@ -1,8 +1,21 @@
 import org.junit.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TestStringMatches {
     @Test
     public void testStringMatches() {
-        System.out.println("abc".matches("..."));
+        print("abc".matches("..."));
+
+        Pattern pattern = Pattern.compile("[a-z]{3}");
+        Matcher matcher = pattern.matcher("abc");
+        print(matcher.matches());
+
+        print(pattern.pattern());
+    }
+
+    public static void print(Object o){
+        System.out.println(o);
     }
 }
