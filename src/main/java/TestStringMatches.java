8c20a2e2daa226b498eaad1670bf24868aaa0470
Monkey <1466947023@qq.com>
2018-05-26 12:44:01 +0800

    认识Pattern中匹配规则

--- src/main/java/TestStringMatches.java
+++ src/main/java/TestStringMatches.java
@@ -15,6 +15,25 @@ public class TestStringMatches {
         print(pattern.pattern());
     }
 
+    @Test
+    public void testPattern() {
+        print("aaa".matches("a.."));//.表示任意字符
+        print("aaa".matches("a+"));//一次或者多次
+        print("aaa".matches("a?"));//一次或者没有
+        print("aaa".matches("a*"));//0次或者多次
+        print("".matches("a*"));//空串当然是匹配的
+        print("".matches("a?"));//同上
+        print("aaaaaa".matches("a{1,3}"));//一次到三次
+        print("aaa".matches("a{1,}"));
+        print("检测IP地址：");
+        //错误的正则表达式
+        print("119.29.62.18".matches("//d//d//d//.//d//d//d//.//d//d//d//.//d//d//d"));
+    }
+
+    /**
+     * 辅助打印函数
+     * @param o 要打印的对象
+     */
     public static void print(Object o){
         System.out.println(o);
     }
